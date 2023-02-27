75696ee804ed1b1278231c7f1d582c20c54a154a
Netanel Ben <netanelben@users.noreply.github.com>
2023-02-27 15:24:28 +0200

    `NFTItemCard` add select checkbox and styling (#499)

--- packages/web3-redux-components/src/components/NFTItemCard/NFTItemCard.stories.tsx
+++ packages/web3-redux-components/src/components/NFTItemCard/NFTItemCard.stories.tsx
@@ -1,22 +1,36 @@
-import { ComponentStory, ComponentMeta } from '@storybook/react';
-import NFTItemCard from '.';
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import NFTItemCard from ".";
 
 const Wrapper = (props: any) => {
     return <NFTItemCard {...props} />;
 };
 
 export default {
-    title: 'NFT/NFTItemCard',
+    title: "NFT/NFTItemCard",
     component: NFTItemCard,
 } as ComponentMeta<typeof NFTItemCard>;
 
-const Template: ComponentStory<typeof NFTItemCard> = (args: any) => <Wrapper {...args} />;
+const Template: ComponentStory<typeof NFTItemCard> = (args: any) => (
+    <Wrapper {...args} />
+);
 
-export const Main = Template.bind({});
+export const Breeding = Template.bind({});
 
-Main.args = {
-    itemName: '8250',
-    tokenName: 'ERC721',
+Breeding.args = {
+    itemName: "Wings Have",
+    tokenName: "ERC721",
     generateTime: 600,
-    assetPreviewSrc: 'http://placehold.jp/168x168.png',
+    assetPreviewSrc: "http://xxx",
+};
+
+export const Asset = Template.bind({});
+
+Asset.args = {
+    tokenId: 1,
+    value: 0.276,
+    itemName: "Wings Have",
+    assetPreviewSrc: "http://xxx",
+    isSelectable: true,
+    isSelected: true,
+    onSelect: console.log,
 };
--- packages/web3-redux-components/src/components/NFTItemCard/index.tsx
+++ packages/web3-redux-components/src/components/NFTItemCard/index.tsx
@@ -1,37 +1,100 @@
-import { useTheme, Box, Image, Flex } from '@chakra-ui/react';
-import Icon from '../Icon/index.js';
+import { useTheme, Box, Image, Flex, Center, Checkbox } from "@chakra-ui/react";
+import Icon from "../Icon/index.js";
+import { ReactComponent as BrokenImgIcon } from "./assets/broken-img-icon.svg";
 
 export interface NFTItemCardProps {
+    tokenId?: number;
+    value?: number;
     itemName?: string;
     tokenName?: string;
-    generateTime?: number;
+    generateTime?: number | undefined;
     assetPreviewSrc?: string;
+    isSelectable?: boolean;
+    isSelected?: boolean;
+    onSelect?: Function;
 }
 
 export const NFTItemCard = ({
+    value,
     itemName,
     tokenName,
-    generateTime = 1,
-    assetPreviewSrc = 'http://placehold.jp/228x196.png',
+    generateTime,
+    assetPreviewSrc,
+    isSelectable,
+    isSelected = false,
+    onSelect,
 }: NFTItemCardProps) => {
     const { themes } = useTheme();
 
     return (
-        <Box p={'8px 12px'} w={'168px'} borderRadius={4} bg={themes.color6} boxShadow={'lg'}>
-            <Box marginBottom={2} w={'100%'} h={'108px'} overflow={'hidden'} borderRadius={4}>
-                <Image src={assetPreviewSrc} w={'100%'} objectFit={'scale-down'} />
+        <Box
+            p={2}
+            boxSize={168}
+            borderRadius={4}
+            bg={themes.color6}
+            boxShadow={"lg"}
+            pos={"relative"}
+            border={isSelected ? `1px solid ${themes.color1}` : "none"}
+        >
+            {isSelectable && (
+                <Checkbox
+                    borderRadius={"50%"}
+                    right={2}
+                    position={"absolute"}
+                    defaultChecked={isSelected}
+                    onChange={onSelect}
+                />
+            )}
+            <Box
+                w={"100%"}
+                h={"108px"}
+                overflow={"hidden"}
+                borderRadius={4}
+                mb={2}
+            >
+                <Image
+                    w={"100%"}
+                    h={"100%"}
+                    bg={themes.color7}
+                    src={assetPreviewSrc}
+                    fallback={
+                        <Center w={"100%"} h={"100%"}>
+                            <Box boxSize={20}>
+                                <BrokenImgIcon />
+                            </Box>
+                        </Center>
+                    }
+                />
             </Box>
 
-            <Box color={themes.color7} marginBottom={1} w={'100%'} fontWeight={700} fontSize={14}>
+            <Box
+                color={themes.color7}
+                marginBottom={1}
+                w={"100%"}
+                fontWeight={700}
+                fontSize={14}
+            >
                 {itemName}
             </Box>
 
-            <Flex justifyContent={'space-between'} alignItems={'center'} color={themes.color9} fontSize={12}>
-                <Box>{tokenName}</Box>
-                <Flex alignItems={'center'}>
-                    <Icon icon={'Clock'} size={18} mr={1} />
-                    Slow ({Math.floor(generateTime / 60)})m
-                </Flex>
+            <Flex
+                justifyContent={"space-between"}
+                alignItems={"center"}
+                color={themes.color9}
+                fontSize={12}
+            >
+                {value && (
+                    <Box fontSize={12} color={themes.color7}>
+                        {value}
+                    </Box>
+                )}
+                {tokenName && <Box>{tokenName}</Box>}
+                {generateTime && (
+                    <Flex alignItems={"center"}>
+                        <Icon icon={"Clock"} size={18} mr={1} />
+                        Slow ({Math.floor(generateTime / 60)})m
+                    </Flex>
+                )}
             </Flex>
         </Box>
     );
